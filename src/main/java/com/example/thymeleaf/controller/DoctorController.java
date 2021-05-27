62090f10d6a57838eff926218a49d0dab6870ece
ergunmu1 <mustafa.ergun@natixis.com>
2021-05-27 12:01:07 +0200

    update doctor with thymeleaf

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -1,6 +1,8 @@
 package com.example.thymeleaf.controller;
 
+import com.example.thymeleaf.model.Doctor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DoctorController {
 
     @GetMapping("/doctor/")
-    public String doctor(@RequestParam(required=false, defaultValue="0") int number,
+    public String doctor(Model model, @RequestParam(required=false, defaultValue="0") int number,
                          @RequestParam(required=false, defaultValue="John Smith") String name) {
 
+        // doctor = new Doctor(number,name);
+        System.out.println("CONTROLLER");
+        model.addAttribute("doctorname", name);
+        model.addAttribute("doctornumber", number);
         return "doctor";
     }
 }
